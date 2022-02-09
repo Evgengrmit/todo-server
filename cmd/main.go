c3ceb2cc0110bb2d6e2d95c7d680645ee630d0ce
Evgengrmit <evgengrmit@gmail.com>
2022-02-09 23:19:34 +0300

    add creating db

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	"todo"
 	"todo/pkg/handler"
@@ -9,11 +10,20 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error occurred when initialization configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	serv := service.NewServer(repos)
 	handl := handler.NewHandler(serv)
 	srv := new(todo.MyServer)
-	if err := srv.Run("8080", handl.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handl.InitRoutes()); err != nil {
 		log.Fatalf("error occurred when starting the server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
